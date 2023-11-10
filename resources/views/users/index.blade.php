167b7eebbc8e33540e51ed65a4647ab79f08cb58
honest888 <dufgu888@gmail.com>
2023-11-10 15:58:34 +0200

    update title

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -62,7 +62,7 @@ The above copyright notice and this permission notice shall be included in all c
     <meta property="og:site_name" content="Creative Tim" />
     
     <title>
-        {{ __('Paper Dashboard by Creative Tim') }}
+        {{ __('Joist') }}
     </title>
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no'
         name='viewport' />
