0cbfe896b423f903e3568e73c5661e28f4b6fceb
superstar0718 <bestdeveloper0718@gmail.com>
2023-11-17 07:42:34 -0800

    update

---
+++ resources/js/views/pages/estimates/item.vue
@@ -0,0 +1,19 @@
+<script setup>
+import ItemInput from "./ItemInput.vue";
+</script>
+<template>
+  <div class="d-flex">
+    <VCol cols="6" class="item-box">
+      <ItemInput classname="pl-5" type="text" placeholder="description" />
+    </VCol>
+    <VCol cols="2" class="item-box">
+      <ItemInput />
+    </VCol>
+    <VCol cols="2" class="item-box">
+      <ItemInput />
+    </VCol>
+    <VCol cols="2" class="item-box">
+      <div>$4000</div>
+    </VCol>
+  </div>
+</template>
